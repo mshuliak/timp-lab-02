a12c03acaf75e9be7bdd7f86aa656d512d6c8ab4
Robert29000 <mayka29000@mail.ru>
2020-04-07 21:42:35 +0300

    Added comments

--- Hello world.cpp
+++ Hello world.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char** argv)
 {
- string name;
- cin >> name;
+ string name;  // User name
+ cin >> name;  // Input user name
  std::cout << "Hello world from " << name << std::endl;
 }
